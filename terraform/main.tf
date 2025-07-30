3ded6acaa9edb3cb8ca4c7fbf2c330b96db0e363
root <root@ip-172-31-16-203.ap-southeast-2.compute.internal>
2025-07-30 13:16:01 +0000

    this is updated version V2

---
+++ terraform/main.tf
@@ -0,0 +1,55 @@
+provider "aws" {
+  region     = "ap-southeast-2a"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "aws_key_pair" "monitoring" {
+  key_name   = "monitoring-key"
+  public_key = file("/home/ec2-user/.ssh/monitoring_key.pub")
+}
+
+resource "aws_security_group" "monitoring_sg" {
+  name        = "monitoring-sg"
+  description = "Allow SSH, Prometheus, Grafana"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "rhel_monitor" {
+  ami                    = "ami-0705fe1e9a50e0d57" # RHEL 9 in us-east-1
+  instance_type          = "t3.micro"
+  key_name               = aws_key_pair.monitoring.key_name
+  vpc_security_group_ids = [aws_security_group.monitoring_sg.id]
+
+  tags = {
+    Name = "monitoring-rhel"
+  }
+}
+
---
+++ terraform/output.tf
@@ -0,0 +1,4 @@
+output "instance_ip" {
+  value = aws_instance.rhel_monitor.public_ip
+}
+
